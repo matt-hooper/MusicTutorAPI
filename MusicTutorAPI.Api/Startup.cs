297a33a50307fd97570ba4e19b909bc2e75c5f3d
Matthew Hooper <mhooper@scottlogic.com>
2020-03-04 20:09:41 +0000

    Add NSwag (instead of Swashbuckle)

--- MusicTutorAPI.Api/Startup.cs
+++ MusicTutorAPI.Api/Startup.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using GenericServices.Configuration;
+using GenericServices.Setup;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -11,7 +14,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+//using Microsoft.OpenApi.Models;
+using MusicTutorAPI.Core.Dtos;
 using MusicTutorAPI.Data;
 
 namespace MusicTutorAPI.Api
@@ -33,11 +37,25 @@ namespace MusicTutorAPI.Api
             
             services.AddDbContext<MusicTutorAPIDbContext>(opt =>
                opt.UseInMemoryDatabase("MusicTutorFull"));
-            
-            services.AddSwaggerGen(options =>
+
+            services.GenericServicesSimpleSetup<MusicTutorAPIDbContext>(new GenericServicesConfig
             {
-                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Music Tutor", Version = "v1" });
+                DtoAccessValidateOnSave = true,     //we use  Dto access for Create/Update
+                DirectAccessValidateOnSave = true  //And direct access for Delete                
+            }, Assembly.GetAssembly(typeof(CreateInstrumentDto)));   
+
+            // Register the Swagger services
+            services.AddSwaggerDocument(config =>
+            {
+                //config.GeneratorSettings.OperationProcessors.TryGet<ApiVersionProcessor>().IncludedVersions = new[] { "1.0" };
+                //config.SwaggerRoute = "v1.0.json";
+                config.Title = "Music Tutor V1";                
             });
+            
+            // services.AddSwaggerGen(options =>
+            // {
+            //     options.SwaggerDoc("v1", new OpenApiInfo { Title = "Music Tutor", Version = "v1" });
+            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,13 +77,19 @@ namespace MusicTutorAPI.Api
                 endpoints.MapControllers();
             });
 
-            app.UseSwagger();
-
-            app.UseSwaggerUI(c =>
+            app.UseOpenApi();
+            app.UseSwaggerUi3(c =>
             {
-                c.RoutePrefix = "";
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Music Tutor V1");
+                c.DocumentTitle = "Music Tutor V1";
             });
+
+            // app.UseSwagger();
+
+            // app.UseSwaggerUI(c =>
+            // {
+            //     c.RoutePrefix = "";
+            //     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Music Tutor V1");
+            // });
         }
     }
 }
