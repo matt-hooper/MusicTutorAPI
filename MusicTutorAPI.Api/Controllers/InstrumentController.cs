e285d93a3ea3d76bade4ac35fd71e0db5990e01a
matt-hooper <mhooper@scottlogic.com>
2020-03-08 14:56:46 +0000

    Complete Instrument Controller

--- MusicTutorAPI.Api/Controllers/InstrumentController.cs
+++ MusicTutorAPI.Api/Controllers/InstrumentController.cs
@@ -17,20 +17,23 @@ namespace MusicTutorAPI.Api.Controllers
     [Route("[controller]")]
     public class InstrumentController : ControllerBase
     {
-        
+
         private readonly ILogger<InstrumentController> _logger;
         private readonly MusicTutorAPIDbContext _context;
 
-        public InstrumentController(ILogger<InstrumentController> logger, MusicTutorAPIDbContext musicTutorAPIDbContext)
+        private readonly ICrudServicesAsync _service;
+
+        public InstrumentController(ILogger<InstrumentController> logger, MusicTutorAPIDbContext musicTutorAPIDbContext, ICrudServicesAsync service)
         {
             _logger = logger;
             _context = musicTutorAPIDbContext;
+            _service = service;
         }
 
         [HttpGet]
-        public async Task<ActionResult<WebApiMessageAndResult<List<Instrument>>>> GetManyAsync([FromServices]ICrudServices service)
+        public async Task<ActionResult<WebApiMessageAndResult<List<Instrument>>>> GetManyAsync()
         {
-            return service.Response(await service.ReadManyNoTracked<Instrument>().ToListAsync());
+            return _service.Response(await _service.ReadManyNoTracked<Instrument>().ToListAsync());
         }
 
         /// <summary>
@@ -40,11 +43,10 @@ namespace MusicTutorAPI.Api.Controllers
         /// <param name="service"></param>
         /// <returns></returns>
         [HttpGet("{id}", Name = "GetSingleInstrument")]
-        public async Task<ActionResult<WebApiMessageAndResult<Instrument>>> GetSingleAsync(int id, [FromServices]ICrudServicesAsync service)
+        public async Task<ActionResult<WebApiMessageAndResult<Instrument>>> GetSingleAsync(int id)
         {
-            return service.Response(await service.ReadSingleAsync<Instrument>(id));
+            return _service.Response(await _service.ReadSingleAsync<Instrument>(id));
         }
-
 
         /// <summary>
         /// Creates a new item and returns the created entity, with the Id value provided by the database
@@ -55,24 +57,49 @@ namespace MusicTutorAPI.Api.Controllers
         /// <returns>If successful it returns a CreatedAtRoute response - see
         /// https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api?view=aspnetcore-2.1#implement-the-other-crud-operations
         /// </returns>
-        [ProducesResponseType(typeof (CreateInstrumentDto), 201)] //You need this, otherwise Swagger says the success status is 200, not 201
+        [ProducesResponseType(typeof(CreateInstrumentDto), 201)] //You need this, otherwise Swagger says the success status is 200, not 201
+        [ProducesResponseType(typeof(string), 400)]
         [HttpPost]
-        public async Task<ActionResult<CreateInstrumentDto>> PostAsync(CreateInstrumentDto item, [FromServices]ICrudServicesAsync service)
+        public async Task<ActionResult<CreateInstrumentDto>> PostAsync(CreateInstrumentDto item)
         {
-                        
-            try 
+
+            try
             {
-                var result = await service.CreateAndSaveAsync(item);
+                var result = await _service.CreateAndSaveAsync(item);
                 //NOTE: to get this to work you MUST set the name of the HttpGet, e.g. [HttpGet("{id}", Name= "GetSingleTodo")],
                 //on the Get you want to call, then then use the Name value in the Response.
                 //Otherwise you get a "No route matches the supplied values" error.
                 //see https://stackoverflow.com/questions/36560239/asp-net-core-createdatroute-failure for more on this
-                return service.Response(this, "GetSingleInstrument", new { id = result.Id },  result);
+                return _service.Response(this, "GetSingleInstrument", new { id = result.Id }, result);
             }
             catch (DbUpdateException ex)
             {
                 return BadRequest(ex.InnerException.Message);
             }
+        }
+
+        /// <summary>
+        /// Updates the Name. 
+        /// </summary>
+        /// <param name="dto">dto containing Id and Name</param>
+        [Route("name")]
+        [HttpPatch()]
+        public async Task<ActionResult<WebApiMessageOnly>> Name(CreateInstrumentDto dto)
+        {
+            await _service.UpdateAndSaveAsync(dto);
+            return _service.Response();
+        }
+
+        /// <summary>
+        /// Delete the Instrument 
+        /// </summary>
+        /// <returns></returns>
+        // DELETE api/instrument/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<WebApiMessageOnly>> DeleteItemAsync(int id)
+        {
+            await _service.DeleteAndSaveAsync<Instrument>(id);
+            return _service.Response();
         }
     }
 }
